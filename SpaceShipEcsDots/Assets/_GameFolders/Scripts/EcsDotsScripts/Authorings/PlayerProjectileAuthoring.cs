c57fafc2106ae9f4f76d93cbe617c2cef5937ce2
berkterek <42406263+berkterek@users.noreply.github.com>
2023-09-24 11:56:10 +0300

    feat: player projectile created

---
+++ SpaceShipEcsDots/Assets/_GameFolders/Scripts/EcsDotsScripts/Authorings/PlayerProjectileAuthoring.cs
@@ -0,0 +1,18 @@
+using Unity.Entities;
+using UnityEngine;
+
+namespace SpaceShipEcsDots.Authorings
+{
+    public class PlayerProjectileAuthoring : MonoBehaviour
+    {
+        
+    }   
+    
+    public class PlayerProjectileBaker : Baker<PlayerProjectileAuthoring>
+    {
+        public override void Bake(PlayerProjectileAuthoring authoring)
+        {
+            var entity = GetEntity(TransformUsageFlags.Dynamic);
+        }
+    }
+}
